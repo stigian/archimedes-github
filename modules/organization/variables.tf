f2fb8ac1a55b29c25790ebac2ad9465f3dffb7a7
lancehampton <lance@stigian.com>
2024-06-18 09:42:54 -0400

    update var names

--- modules/organization/variables.tf
+++ modules/organization/variables.tf
@@ -1,45 +1,45 @@
 ###############################################################################
 # Organization Variables
 ###############################################################################
-variable "org_billing_email" {
+variable "billing_email" {
   description = "The billing email for the GitHub organization"
   type        = string
 }
 
-variable "org_company_name" {
+variable "company_name" {
   description = "The name of the company owning/operating the GitHub organization"
   type        = string
 }
 
-variable "org_blog_url" {
+variable "blog_url" {
   description = "The URL of the GitHub organization's blog"
   type        = string
   default     = null
 }
 
-variable "org_email" {
+variable "email" {
   description = "The contact email for the GitHub organization"
   type        = string
 }
 
-variable "org_twitter_username" {
+variable "twitter_username" {
   description = "The Twitter username for the GitHub organization"
   type        = string
   default     = null
 }
 
-variable "org_location" {
+variable "location" {
   description = "The location of the company or organization"
   type        = string
   default     = null
 }
 
-variable "org_name" {
+variable "name" {
   description = "The display name of the GitHub organization"
   type        = string
 }
 
-variable "org_description" {
+variable "description" {
   description = "The description of the GitHub organization"
   type        = string
 }
